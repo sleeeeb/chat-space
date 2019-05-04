518eebd7646149861af71c4321f033909cdc874c
sleeeeb <nagasoe0326@gmail.com>
2019-05-04 18:53:52 +0900

    delete commentout

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,12 +9,9 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      # redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
       respond_to do |format|
       format.html { redirect_to group_messages_path(@group)  }
       format.json
-      # flash.now[:alert] = 'メッセージが送信されました'
-      # notice: 'メッセージが送信されました'
       end
 
     else
